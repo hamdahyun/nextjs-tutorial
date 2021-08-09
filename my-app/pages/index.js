7d3f0bab9727e94c812d9c77cc7444ac3644c7ea
dahyun Ham <dahuyn1@gmail.com>
2021-08-09 23:25:41 +0900

    axios

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,6 +1,18 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const API_URL = 'http://markup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
+  function getData() {
+    Axios.get(API_URL).then( res => {
+      console.log(res.data)
+    })
+  }
+  useEffect(()=> {
+    getData();
+  }, []);
+
   return (
     <div>
       <Head>
