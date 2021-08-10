9f8c26321ac851ed262e96231aeba30cdcaedcb6
dahyun Ham <dahuyn1@gmail.com>
2021-08-10 22:55:56 +0900

    add itemList

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import itemList from '../src/components/itemList';
 
 export default function Home() {
+  const [list, setlist] = useState([]);
   const API_URL = 'http://markup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 
   function getData() {
     Axios.get(API_URL).then( res => {
-      console.log(res.data)
+      console.log(res.data);
+      setlist(res.data)
     })
   }
   useEffect(()=> {
@@ -18,7 +22,7 @@ export default function Home() {
       <Head>
         <title>Home | dahyun</title>
       </Head>
-
+      <itemList list={list} />
     </div>
   )
 }
