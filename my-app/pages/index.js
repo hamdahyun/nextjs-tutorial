7b96acf813303624a623b17e977412896a511350
dahyun Ham <dahuyn1@gmail.com>
2021-08-12 21:19:15 +0900

    api url modify

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import itemList from '../src/components/itemList';
+import ItemList from '../src/components/itemList';
+import { Divider, Header } from 'semantic-ui-react';
 
 export default function Home() {
   const [list, setlist] = useState([]);
-  const API_URL = 'http://markup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+  const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 
   function getData() {
     Axios.get(API_URL).then( res => {
@@ -22,7 +23,9 @@ export default function Home() {
       <Head>
         <title>Home | dahyun</title>
       </Head>
-      <itemList list={list} />
+      <Header as="h3">베스트 상품</Header>
+      <Divider />
+      <ItemList list={list} />
     </div>
   )
 }
--- my-app/src/components/itemList.js
+++ my-app/src/components/itemList.js
@@ -1,3 +1,15 @@
+import { Grid } from "semantic-ui-react";
+
 export default function itemList({list}) {
-    return(<div>list</div>);
+    return(<div>
+        <Grid columns={3} divided>
+            <Grid.Row>
+                {list.map(item => (
+                    <Grid.Column>
+                        <img src={item.image_link} alt={item.name} />
+                    </Grid.Column>
+                ))}
+            </Grid.Row>
+        </Grid>
+    </div>);
 }
